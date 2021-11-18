a9ebd8d404dc03dcad25e54a51efe0d0e30d06d7
Satwik Kambham <satwik.kambham@gmail.com>
2021-11-18 18:49:50 +0530

    Added database connectivity helper class and project info class which stores info on projects

---
+++ DBHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LiteDB;
+
+namespace Project_Manager_V2
+{
+    internal class DBHelper
+    {
+        private LiteDatabase db;
+        private ILiteCollection<ProjectInfo> table;
+
+        public DBHelper()
+        {
+            // Open database or create database if it does not exist
+            db = new LiteDatabase(@".\DB.db");
+            table = db.GetCollection<ProjectInfo>("ProjectInfos");
+        }
+
+        // Get Project Info by name
+        public ProjectInfo GetProjectInfo(string Name)
+        {
+            return table.FindOne(x => x.Name == Name);
+        }
+
+        // Insert or Update Project Info
+        public void SetProjectInfo(ProjectInfo projectInfo)
+        {
+            table.Upsert(projectInfo);
+        }
+    }
+}
---
+++ ProjectInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Project_Manager_V2
+{
+    public class ProjectInfo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Tags { get; set; }
+        public List<string[]> ScreenshotPaths { get; set; }
+        public List<string[]> ToDos { get; set; }
+    }
+}
